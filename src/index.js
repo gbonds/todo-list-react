d82e2b7faa7fabe3a6052489ee1e8aaae82bb794
gbonds <ge.bonds@gmail.com>
2021-02-10 18:29:40 -0500

    Created component for todo list items

--- src/index.js
+++ src/index.js
@@ -4,8 +4,14 @@ import './index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
+const DATA = [
+  { id: "todo-0", name: "Eat", completed: true},
+  { id: "todo-1", name: "Sleep", completed: false},
+  { id: "todo-2", name: "Code", completed: false}
+];
+
 ReactDOM.render(
-  <App subject="Bob" />,
+  <App tasks={DATA} />,
   document.getElementById('root')
 );
 
