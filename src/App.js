6d57e153d09fc02184b5348937718a00355c5a67
gbonds <ge.bonds@gmail.com>
2021-02-11 12:55:07 -0500

    Added interactivity to form and task list

--- src/App.js
+++ src/App.js
@@ -1,28 +1,68 @@
+import React, {useState} from "react";
+import {nanoid} from "nanoid";
+
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
 
+// TODO further separate out components into header, form, and list
+
 function App(props) {
-  const taskList = props.tasks.map(task => (
-    <Todo 
-      id={task.id} 
-      name={task.name} 
-      completed={task.completed}
-      key={task.id}
+  const [tasks, setTasks] = useState(props.tasks);
+
+  function addTask(name) {
+    const newTask = {
+      id: "todo-" + nanoid(),
+      name: name,
+      completed: false
+    };
+    setTasks([...tasks, newTask]);
+  };
+
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map(task => {
+      // if task has same ID as edited task
+      if (id===task.id) {
+        // make new object with inverted completed prop
+        return {...task, completed: !task.completed}
+      }
+      // if no match, return original task without edits
+      return task;
+    });
+    setTasks(updatedTasks);
+  };
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter(task => id !== task.id);
+    setTasks(remainingTasks);
+  };
+
+  const taskList = tasks.map(task => (
+    <Todo
+        id={task.id}
+        name={task.name}
+        completed={task.completed}
+        key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask = {deleteTask}
       />
-  ));
+    )
+  );
+
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+  const listHeadingText = `${taskList.length} ${tasksNoun} remaining`;
 
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <Form />
+      <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
         <FilterButton />
         <FilterButton />
         <FilterButton />
       </div>
       <h2 id="list-heading">
-        3 tasks remaining
+        {listHeadingText}
       </h2>
       <ul
         role="list"
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,23 +1,39 @@
+import React, { useState } from "react";
+
 function Form(props) {
-    return (
-        <form>
-            <h2 className="label-wrapper">
-                <label htmlFor="new-todo-input" className="label__lg">
-                    What needs to be done?
-          </label>
-            </h2>
-            <input
-                type="text"
-                id="new-todo-input"
-                className="input input__lg"
-                name="text"
-                autoComplete="off"
-            />
-            <button type="submit" className="btn btn__primary btn__lg">
-                Add
-        </button>
-        </form>
-    );
+  const [name, setName] = useState("");
+
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    // TODO add function to prevent blank to-do submission
+    e.preventDefault();
+    props.addTask(name);
+    setName(""); // empties out input field after submission
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2 className="label-wrapper">
+        <label htmlFor="new-todo-input" className="label__lg">
+          What needs to be done?
+        </label>
+      </h2>
+      <input
+        type="text"
+        id="new-todo-input"
+        className="input input__lg"
+        name="text"
+        autoComplete="off"
+        value={name}
+        onChange={handleChange}
+      />
+      <button type="submit" className="btn btn__primary btn__lg">
+        Add
+      </button>
+    </form>
+  );
 }
 
 export default Form;
